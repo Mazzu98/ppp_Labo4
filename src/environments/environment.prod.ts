355c64f23f9d4210c6774c2314e19cf34228f2a0
Mazzu98 <juani.mp1@gmail.com>
2021-09-30 11:50:30 -0300

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBNEDTstkDx-z-35LGq8M8s3xKm0LhS5b4",
+    authDomain: "ppp01-89f9d.firebaseapp.com",
+    projectId: "ppp01-89f9d",
+    storageBucket: "ppp01-89f9d.appspot.com",
+    messagingSenderId: "485845432100",
+    appId: "1:485845432100:web:8f04b6c1c5c9287db1e5b1"
+  }
 };
